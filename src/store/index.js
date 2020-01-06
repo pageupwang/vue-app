d64387339caf5e1fc0b24c163418d378e6711ef3
wangjl <wangjl@360pai.com>
2020-01-06 17:02:16 +0800

    提交

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import VuexPersistence from 'vuex-persist'
+import createPersistedState from "vuex-persistedstate";
 
-const vuexSession = new VuexPersistence({
-  storage: window.sessionStorage
-})
 Vue.use(Vuex)
 import user from './modules/user'
 export default new Vuex.Store({
   modules: {
     user: user,
   },
-  plugins: [vuexSession.plugin]
+  plugins: [
+    createPersistedState()
+  ]
 })
